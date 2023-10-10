188ac431352d34ec3352453f7a6358b9e98ea8f2
JanionNevill <janion.nevill@leicester.ac.uk>
2023-10-10 10:43:24 +0100

    Added meta-regression tab. Left commented code for finding covariate names.

--- server.R
+++ server.R
@@ -52,4 +52,29 @@ shinyServer(function(input, output, session) {
   )
   
   user_guide_page_server(id = "user_guide")
+  
+  MetaRegressionTabServer(
+    id = "meta_regression",
+    all_data = data,
+    sub_data = data
+  )
+  
+    
+  shiny::observeEvent(
+    data,
+    {
+      # Check if any covariates in data
+      # if (length(FindCovariateNames(data)) == 0) {
+      shiny::hideTab(
+        inputId = "main_tabs",
+        target = "4. Meta-regression"
+      )
+      # } else {
+      #   shiny::showTab(
+      #     inputId = "main_tabs",
+      #     target = "4. Meta-regression"
+      #   )
+      # }
+    }
+  )
 })
