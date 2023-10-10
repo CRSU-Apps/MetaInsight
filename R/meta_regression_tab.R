71e8c69766429704edf0ed9b09b916713b25e698
JanionNevill <janion.nevill@leicester.ac.uk>
2023-10-10 10:43:24 +0100

    Added meta-regression tab. Left commented code for finding covariate names.

--- R/meta_regression_tab.R
+++ R/meta_regression_tab.R
@@ -1,61 +1,23 @@
-#' Create the meta-regression panel.
-#'
-#' @param id ID of the module
-#' @return Div containing the module
-meta_regression_tab_ui <- function(id) {
+CreateTitle <- function(index) {
+  return(paste0(ifelse(is.null(index), "", paste0(index, ". ")), "Meta-regression"))
+}
+
+MetaRegressionTabUi <- function(id, title) {
   ns <- NS(id)
-  div(
-    fluidPage(
-      tabsetPanel(
-        id = "regression_tabs",
-        tabPanel(
-          title = "4a. Summary",
-          # Add a new module here for the summary panel
-        ),
-        tabPanel(
-          title = "4b. Baseline Risk Analysis"
-          # Add a new module here for the baseline risk analysis panel
-        ),
-        tabPanel(
-          title = "4c. Covariate Analysis",
-          conditionalPanel(
-            condition = "!output.has_covariates",
-            ns = ns,
-            div(
-              h3(
-                "No covariate data. To add covariate data, add a column titled",
-                code("covar.*"),
-                "where the",
-                code("*"),
-                "is replaced by the covariate name. eg. ",
-                code("covar.age")
-              ),
-              style = "color: red; font-style: italic; font-weight: bold;"
-            )
-          ),
-          conditionalPanel(
-            condition = "output.has_covariates",
-            ns = ns,
-            covariate_analysis_panel_ui(id = ns("covariate_analysis"))
-          )
-        )
-      )
-    )
+  shiny::tabPanel(
+    title = title,
+    shiny::selectInput(
+      inputId = ns("covariate_box"),
+      label = "Select covariate",
+      choices = c()
+    ),
+    style = "class: 'main'"
   )
 }
 
-#' Create the meta-regression server.
-#'
-#' @param id ID of the module
-#' @param all_data Study data including covariate columns, in wide or long format
-meta_regression_tab_server <- function(id, all_data) {
+MetaRegressionTabServer <- function(id, all_data, sub_data) {
   shiny::moduleServer(id, function(input, output, session) {
-    
-    output$has_covariates <- reactive({
-      length(FindCovariateNames(all_data())) > 0
-    })
-    shiny::outputOptions(x = output, name = "has_covariates", suspendWhenHidden = FALSE)
-  
-    covariate_analysis_panel_server(id = "covariate_analysis", all_data = all_data)
+    # shiny::updateSelectInput(inputId = "covariate_box", choices = FindCovariateNames(all_data))
+    covariate = shiny::reactive({ input$covariate_box })
   })
 }
