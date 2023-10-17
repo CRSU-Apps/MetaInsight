7a7a7d833250b1377bdf7d88855eac2157729b86
Clareece Nevill <clareece.nevill@gmail.com>
2023-10-17 12:12:25 +0100

    Created gemtc_analysis.R file

---
+++ R/gemtc_analysis.R
@@ -0,0 +1,8 @@
+# User-written functions to wrap up functionality related to running models with {gemtc} #
+
+
+# Function for formatting standard app user data into a {gemtc} 'network' object
+
+CreateGemtcObject <- function(){
+  
+}
