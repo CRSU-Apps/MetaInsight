b468d596d2e1d36dc655ecc208194b93a6be4cc8
Naomi Bradbury <98603626+nabury@users.noreply.github.com>
2023-10-20 16:57:11 +0100

    Created function to make covariate summary plot

--- R/CovariatePanel1d.R
+++ R/CovariatePanel1d.R
@@ -15,42 +15,43 @@ load_covariate_tab_ui <- function(id) {
 #' Module server for the covariate summary plot tab.
 #'
 #' @param id ID of the module
-#' @param data Study data in BUGSnet format - to be added
+#' @param BUGSnet_data Study data in BUGSnet format - to be added
 #' @param covariate Covariate name - to be added
 #' @return Covariate plot from BUGSnet::data.plot
 #' 
 load_covariate_tab_server <- function(id) {
   moduleServer(id, function(input, output, session) {
     
-    # Testing text
+    # Testing text 
     output$text <- renderPrint({
       "foo"
     })
     
-    # Create covariate plot
-    output$covariate_plot <- renderPlot({
+    # BUGSnet data.plot used for covariate summary
+    # https://rdrr.io/github/audrey-b/BUGSnet/man/data.plot.html
+    make_covariate_plot <- function(BUGSnet_data, covariate) {
 
-      # Hard-code BUGSnet data prep of diabetes.sim data (to be generalised when data upload sorted)
-      BUGSnet_data_prep <- BUGSnet::data.prep(arm.data = BUGSnet::diabetes.sim,
-                                              varname.t = "Treatment",
-                                              varname.s = "Study")
-
-      # Hard-coded covariate - will become an input
-      covariate <- "age"
-
-      # BUGSnet Covariate plot
-      # https://rdrr.io/github/audrey-b/BUGSnet/man/data.plot.html
-      return(BUGSnet::data.plot(BUGSnet_data_prep,
+      return(BUGSnet::data.plot(BUGSnet_data,
                                 covariate = covariate, # Covariate name is automatically used for y-axis label
                                 # half.length = "age_SD", # Error bars - needs a second covariate, possible future addition
                                 by = "treatment", 
-                                text.size = 16 # May need to be reactive - test with different size datasets
-                                )
-      )
+                                text.size = 16) # May need to be reactive - test with different size datasets
+             )
+    }
+    
+    # Render covariate summary plot
+    output$covariate_plot <- renderPlot({
+
+      # Hard-code BUGSnet data prep of diabetes.sim data (will be an input to module when data upload sorted)
+      BUGSnet_data <- BUGSnet::data.prep(arm.data = BUGSnet::diabetes.sim,
+                                              varname.t = "Treatment",
+                                              varname.s = "Study")
+
+      # Hard-coded covariate - will become an input to module
+      covariate <- "age"
+
+      make_covariate_plot(BUGSnet_data, covariate)
     })
-    
-    
-    
     
   })
 }
