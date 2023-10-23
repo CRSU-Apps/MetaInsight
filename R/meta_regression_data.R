0903e6d75732ce9a524d4096df5ceb40834c070c
JanionNevill <janion.nevill@leicester.ac.uk>
2023-10-23 16:12:14 +0100

    Updated meta-regression tab to contain drop-down box for selecting the covariate type which is populated with an inferred type.

--- R/meta_regression_data.R
+++ R/meta_regression_data.R
@@ -21,8 +21,8 @@
   }
 }
 
-#' Validate the covariate and infer the type of the covariate from the data in the column.
-#'  In error cases, this function will throw exceptions:
+#' Infer the type of the covariate from the data in the column. In error cases, this
+#' function will throw exceptions:
 #' - If the data has any NAs
 #' - If the data has any non-numeric values
 #' - If every study has the same covariate value
@@ -31,26 +31,23 @@
 #' @param data Data frame containing all study data.
 #' @param covariate_title Name of the covariate column.
 #'
-#' @return "binary" if covariate has only 0 & 1 as numeric values,
+#' @return "binary" if covariate has only 2 numeric values,
 #' "continuous" if covariate has more than 2 numeric values
-ValidateAndInferCovariateType <- function(data, covariate_title) {
+InferCovariateType <- function(data, covariate_title) {
   covariate_data <- data[[covariate_title]]
-  
-  if (!is.numeric(covariate_data)) {
-    stop("One or more covariate values are non-numerical.")
-  }
   
   covariate_values <- list()
   for (study in unique(data$Study)) {
-    covariate_values[[study]] <- unique(covariate_data[data$Study == study])
+    study_covariate_values <- unique(covariate_data[data$Study == study])
+    covariate_values[[study]] <- study_covariate_values[!is.na(study_covariate_values)]
   }
   
   .ThrowErrorForMatchingStudies(
     values = covariate_values,
     condition = function(study_values) {
-      any(is.na(study_values))
+      length(study_values) == 0
     },
-    message = "Some studies do not define covariate values for all arms:"
+    message = "Some studies do not define covariate values:"
   )
   
   .ThrowErrorForMatchingStudies(
@@ -58,15 +55,120 @@ ValidateAndInferCovariateType <- function(data, covariate_title) {
     condition = function(study_values) {
       length(study_values) > 1
     },
-    message = "Some studies contain inconsistent covariate values between arms:"
+    message = "Some studies contain inconsistent covariate values:"
   )
   
+  .ThrowErrorForMatchingStudies(
+    values = covariate_values,
+    condition = function(study_values) {
+      any(!is.numeric(study_values))
+    },
+    message = "Some studies contain non-numerical covariate values:"
+  )
+
   unique_items <- unique(covariate_data)
   if (length(unique_items) == 1) {
     stop("Cannot analyse covariate with no variation.")
-  } else if (length(unique_items) == 2 && all(sort(unique_items) == c(0, 1))) {
+  } else if (length(unique_items) == 2) {
     return("binary")
+  } else {
+    return("continuous")
   }
-  
-  return("continuous")
 }
+
+#' 
+#' #' Infer the type of the covariate from the data in the column. In error cases, this
+#' #' function will throw exceptions:
+#' #' - If the data has any NAs
+#' #' - If the data has any non-numeric values
+#' #' - If every study has the same covariate value
+#' #' - If any study contains multiple different covariate values
+#' #'
+#' #' @param data Data frame containing all study data.
+#' #' @param covariate_title Name of the covariate column.
+#' #'
+#' #' @return "binary" if covariate has only 2 numeric values,
+#' #' "continuous" if covariate has more than 2 numeric values
+#' InferCovariateType <- function(data, covariate_title) {
+#'   covariate_data <- data[[covariate_title]]
+#'   
+#'   covariate_values <- list()
+#'   inconsistent_studies <- c()
+#'   undefined_studies <- c()
+#'   for (study in unique(data$Study)) {
+#'     study_covariate_values <- unique(covariate_data[data$Study == study])
+#'     study_covariate_values <- study_covariate_values[!is.na(study_covariate_values)]
+#'     covariate_values[[study]] <- study_covariate_values
+#'     if (length(study_covariate_values) == 0) {
+#'       undefined_studies <- c(undefined_studies, study)
+#'     } else if (length(study_covariate_values) > 1) {
+#'       inconsistent_studies <- c(inconsistent_studies, study)
+#'     }
+#'   }
+#'   
+#'   if (length(undefined_studies) > 0) {
+#'     studies_list <- glue::glue_collapse(undefined_studies, sep = ", ")
+#'     stop(paste0("Some studies do not define covariate values: ", studies_list))
+#'   }
+#'   
+#'   if (length(inconsistent_studies) > 0) {
+#'     studies_list <- glue::glue_collapse(inconsistent_studies, sep = ", ")
+#'     stop(paste0("Some studies contain inconsistent covariate values: ", studies_list))
+#'   }
+#'   
+#'   if (any(!is.numeric(unique_items))) {
+#'     stop("Cannot perform meta-regression when covariate values are not numerical.")
+#'   } else if (length(unique_items) == 1) {
+#'     stop("Cannot analyse covariate with no variation.")
+#'   } else if (length(unique_items) == 2) {
+#'     return("binary")
+#'   } else {
+#'     return("continuous")
+#'   }
+#' }
+
+
+#' 
+#' #' Infer the type of the covariate from the data in the column. In error cases, this
+#' #' function will throw exceptions:
+#' #' - If the data has any NAs
+#' #' - If the data has any non-numeric values
+#' #' - If every study has the same covariate value
+#' #' - If any study contains multiple different covariate values
+#' #'
+#' #' @param data Data frame containing all study data.
+#' #' @param covariate_title Name of the covariate column.
+#' #'
+#' #' @return "binary" if covariate has only 2 numeric values,
+#' #' "continuous" if covariate has more than 2 numeric values
+#' InferCovariateType <- function(data, covariate_title) {
+#'   covariate_data <- data[[covariate_title]]
+#'   
+#'   inconsistent_studies <- c()
+#'   for (study in unique(data$Study)) {
+#'     study_covariate_values <- unique(covariate_data[data$Study == study])
+#'     study_covariate_values <- study_covariate_values[!is.na(study_covariate_values)]
+#'     if (length(study_covariate_values) > 1) {
+#'       inconsistent_studies <- c(inconsistent_studies, study)
+#'     }
+#'   }
+#'   
+#'   if (length(inconsistent_studies) > 0) {
+#'     studies_list <- glue::glue_collapse(inconsistent_studies, sep = ", ")
+#'     stop(paste0("Some studies contain inconsistent covariate values: ", studies_list))
+#'   }
+#'   
+#'   unique_items <- unique(covariate_data)
+#'   
+#'   if (any(is.na(unique_items))) {
+#'     stop("Cannot perform meta-regression when some studies are missing the covariate.")
+#'   } else if (any(!is.numeric(unique_items))) {
+#'     stop("Cannot perform meta-regression when covariate values are not numerical.")
+#'   } else if (length(unique_items) == 1) {
+#'     stop("Cannot analyse covariate with no variation.")
+#'   } else if (length(unique_items) == 2) {
+#'     return("binary")
+#'   } else {
+#'     return("continuous")
+#'   }
+#' }
