e79a5b1229335013e8038cc3a44aa130512096ad
JanionNevill <janion.nevill@leicester.ac.uk>
2023-10-23 16:12:14 +0100

    Updated meta-regression tab to contain drop-down box for selecting the covariate type which is populated with an inferred type.

--- tests/testthat/test-meta_regression_data.R
+++ tests/testthat/test-meta_regression_data.R
@@ -1,135 +1,98 @@
 
-test_that("ValidateAndInferCovariateType() throws error when long data has multiple different covariate values", {
+test_that("InferCovariateType() throws error when long data has multiple different covariate values", {
   df <- data.frame(
     Study = c("Jeff et al", "Jeff et al", "Steve and Al", "Steve and Al", "Frank", "Frank"),
     covar.bananas = c(11, 43, 9, 9, 77, 33.33333)
   )
   expect_error(
-    object = ValidateAndInferCovariateType(df, "covar.bananas"),
-    regexp = "Some studies contain inconsistent covariate values between arms: Jeff et al, Frank"
+    object = InferCovariateType(df, "covar.bananas"),
+    regexp = "Some studies contain inconsistent covariate values: Jeff et al, Frank"
   )
 })
 
-test_that("ValidateAndInferCovariateType() throws error when long data has missing covariate values", {
+test_that("InferCovariateType() throws error when long data has missing covariate values", {
   df <- data.frame(
     Study = c("Jeff et al", "Jeff et al", "Steve and Al", "Steve and Al", "Frank", "Frank"),
-    covar.bananas = c(11, 11, NA, NA, NA, NA)
+    covar.bananas = c(11, 43, NA, NA, NA, NA)
   )
   expect_error(
-    object = ValidateAndInferCovariateType(df, "covar.bananas"),
-    regexp = "Some studies do not define covariate values for all arms: Steve and Al, Frank"
-  )
-})
-test_that("ValidateAndInferCovariateType() throws error when long data has missing covariate values for only some arms", {
-  df <- data.frame(
-    Study = c("Jeff et al", "Jeff et al", "Steve and Al", "Steve and Al", "Frank", "Frank"),
-    covar.bananas = c(11, NA, 1, NA, 45, 45)
-  )
-  expect_error(
-    object = ValidateAndInferCovariateType(df, "covar.bananas"),
-    regexp = "Some studies do not define covariate values for all arms: Jeff et al, Steve and Al"
+    object = InferCovariateType(df, "covar.bananas"),
+    regexp = "Some studies do not define covariate values: Steve and Al, Frank"
   )
 })
 
-test_that("ValidateAndInferCovariateType() throws error when wide data has missing covariate values", {
+test_that("InferCovariateType() throws error when wide data has missing covariate values", {
   df <- data.frame(
     Study = c("Jeff et al", "Steve and Al", "Frank"),
     covar.bananas = c(11, NA, NA)
   )
   expect_error(
-    object = ValidateAndInferCovariateType(df, "covar.bananas"),
-    regexp = "Some studies do not define covariate values for all arms: Steve and Al, Frank"
+    object = InferCovariateType(df, "covar.bananas"),
+    regexp = "Some studies do not define covariate values: Steve and Al, Frank"
   )
 })
 
-test_that("ValidateAndInferCovariateType() throws error when long data has non-numerical covariate values", {
+test_that("InferCovariateType() throws error when long data has non-numerical covariate values", {
   df <- data.frame(
     Study = c("Jeff et al", "Jeff et al", "Steve and Al", "Steve and Al", "Frank", "Frank"),
-    covar.bananas = c("11", "11", 9, 9, "77", "77")
+    covar.bananas = c(11, "11", 9, 9, 77, "77")
   )
   expect_error(
-    object = ValidateAndInferCovariateType(df, "covar.bananas"),
-    regexp = "One or more covariate values are non-numerical\\."
+    object = InferCovariateType(df, "covar.bananas"),
+    regexp = "Some studies contain non-numerical covariate values: Jeff et al, Steve and Al"
   )
 })
 
-test_that("ValidateAndInferCovariateType() throws error when wide data has non-numerical covariate values", {
-  df <- data.frame(
-    Study = c("Jeff et al", "Steve and Al", "Frank"),
-    covar.bananas = c("11", 9, "77")
-  )
-  expect_error(
-    object = ValidateAndInferCovariateType(df, "covar.bananas"),
-    regexp = "One or more covariate values are non-numerical\\."
-  )
-})
-
-test_that("ValidateAndInferCovariateType() throws error when long data has all identical covariate values", {
+test_that("InferCovariateType() throws error when long data has non-numerical covariate values", {
   df <- data.frame(
     Study = c("Jeff et al", "Jeff et al", "Steve and Al", "Steve and Al", "Frank", "Frank"),
     covar.bananas = c(11, 11, 11, 11, 11, 11)
   )
   expect_error(
-    object = ValidateAndInferCovariateType(df, "covar.bananas"),
+    object = InferCovariateType(df, "covar.bananas"),
     regexp = "Cannot analyse covariate with no variation\\."
   )
 })
 
-test_that("ValidateAndInferCovariateType() throws error when wide data has all identical covariate values", {
+test_that("InferCovariateType() throws error when wide data has non-numerical covariate values", {
   df <- data.frame(
     Study = c("Jeff et al", "Steve and Al", "Frank"),
     covar.bananas = c(11, 11, 11)
   )
   expect_error(
-    object = ValidateAndInferCovariateType(df, "covar.bananas"),
+    object = InferCovariateType(df, "covar.bananas"),
     regexp = "Cannot analyse covariate with no variation\\."
   )
 })
 
-test_that("ValidateAndInferCovariateType() returns 'binary' when only 0 & 1 in long data", {
-  df <- data.frame(
-    Study = c("Jeff et al", "Jeff et al", "Steve and Al", "Steve and Al", "Frank", "Frank"),
-    covar.bananas = c(0, 0, 1, 1, 0, 0)
-  )
-  expect_equal(ValidateAndInferCovariateType(df, "covar.bananas"), "binary")
-})
-
-test_that("ValidateAndInferCovariateType() returns 'binary' when only 0 & 1 in wide data", {
-  df <- data.frame(
-    Study = c("Jeff et al", "Steve and Al", "Frank"),
-    covar.bananas = c(0, 1, 0)
-  )
-  expect_equal(ValidateAndInferCovariateType(df, "covar.bananas"), "binary")
-})
-
-test_that("ValidateAndInferCovariateType() returns 'continuous' when only 2 values in long data which aren't 0 & 1", {
+test_that("InferCovariateType() returns 'binary' when only 2 values in long data", {
   df <- data.frame(
     Study = c("Jeff et al", "Jeff et al", "Steve and Al", "Steve and Al", "Frank", "Frank"),
     covar.bananas = c(11, 11, 1, 1, 11, 11)
   )
-  expect_equal(ValidateAndInferCovariateType(df, "covar.bananas"), "continuous")
+  expect_equal(InferCovariateType(df, "covar.bananas"), "binary")
 })
 
-test_that("ValidateAndInferCovariateType() returns 'continuous' when only 2 values in wide data which aren't 0 & 1", {
+test_that("InferCovariateType() returns 'binary' when only 2 values in wide data", {
   df <- data.frame(
     Study = c("Jeff et al", "Steve and Al", "Frank"),
     covar.bananas = c(11, 1, 11)
   )
-  expect_equal(ValidateAndInferCovariateType(df, "covar.bananas"), "continuous")
+  expect_equal(InferCovariateType(df, "covar.bananas"), "binary")
 })
 
-test_that("ValidateAndInferCovariateType() returns 'continuous' when more than 2 values in long data", {
+test_that("InferCovariateType() returns 'continuous' when more than 2 values in long data", {
   df <- data.frame(
     Study = c("Jeff et al", "Jeff et al", "Steve and Al", "Steve and Al", "Frank", "Frank"),
     covar.bananas = c(11, 11, 1, 1, 45, 45)
   )
-  expect_equal(ValidateAndInferCovariateType(df, "covar.bananas"), "continuous")
+  expect_equal(InferCovariateType(df, "covar.bananas"), "continuous")
 })
 
-test_that("ValidateAndInferCovariateType() returns 'continuous' when more than 2 values in wide data", {
+test_that("InferCovariateType() returns 'continuous' when more than 2 values in wide data", {
   df <- data.frame(
     Study = c("Jeff et al", "Steve and Al", "Frank"),
     covar.bananas = c(11, 1, 45)
   )
-  expect_equal(ValidateAndInferCovariateType(df, "covar.bananas"), "continuous")
+  expect_equal(InferCovariateType(df, "covar.bananas"), "continuous")
 })
