6c73cdd33770a4b66660c7af36ef20323b6defa8
Clareece Nevill <clareece.nevill@gmail.com>
2023-10-24 15:03:04 +0100

    Tidied functions

--- R/DataWrangling.R
+++ R/DataWrangling.R
@@ -42,11 +42,11 @@ CleanData <- function(data) {
 #' Convert wide format to long format (including covariate columns)
 #' 
 #' @param wide_data Data frame of wide format
-#' @param ConBi Indicator whether outcome is binary or continuous
+#' @param outcome_type Indicator whether outcome is binary or continuous
 #' @return Data frame in long format
-WideToLong <- function(wide_data, ConBi) {
+WideToLong <- function(wide_data, outcome_type) {
   # Specify columns that contain wide data
-  if (ConBi == "Continuous") {
+  if (outcome_type == "Continuous") {
     change_cols <- wide_data %>%
       select(starts_with(c("T","N","Mean","SD")))
   } else {
