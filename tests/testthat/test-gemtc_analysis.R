80f02cfc6630e79ca487d16ba443aeddfa99bbfd
Clareece Nevill <clareece.nevill@gmail.com>
2023-10-26 16:42:38 +0100

    Added unit test
    Amended functions as result

--- tests/testthat/test-gemtc_analysis.R
+++ tests/testthat/test-gemtc_analysis.R
@@ -3,17 +3,18 @@ test_that("PrepDataGemtc() gives correct data for wide binary", {
   data <- read.csv("Binary_wide_cov.csv")
   gemtc_data <- PrepDataGemtc(data, "Binary", "covar.age", "age")
   
-  expected_armData = data.frame(
-    study = c(rep("Constantine", 3), rep("Leo", 3), rep("Justinian", 2)),
-    treatment = c("the Great", "the Younger", "the Dung-named", "the Little", "the Great", "the Butcher", "the Great", "the Slit-nosed"),
-    responders = 30:37,
-    sampleSize = 100:107)
-  expected_studyData = data.frame(
-      study = c("Constantine", "Leo", "Justinian"),
-      age = rep(99, 3))
+  expected_data <- list(
+    armData = data.frame(
+      study=c(rep("Constantine",3), rep("Leo",3), rep("Justinian",2)),
+      treatment=c("the Great", "the Younger", "the Dung-named", "the Little", "the Great", "the Butcher", "the Great", "the Slit-nosed"),
+      responders=30:37,
+      sampleSize=100:107),
+    studyData = data.frame(
+      study=c("Constantine", "Leo", "Justinian"),
+      age=rep(99,3))
+  )
   
-  expect_equal(gemtc_data$armData, expected_armData)
-  expect_equal(gemtc_data$studyData, expected_studyData)
+  expect_equal(gemtc_data, expected_data)
 })
 
 test_that("PrepDataGemtc() gives correct data for long binary", {
@@ -21,17 +22,18 @@ test_that("PrepDataGemtc() gives correct data for long binary", {
   data <- read.csv("Binary_long_cov.csv")
   gemtc_data <- PrepDataGemtc(data, "Binary", "covar.age", "age")
   
-  expected_armData <- data.frame(
-    study = c(rep("Constantine", 3), rep("Leo", 3), rep("Justinian", 2)),
-    treatment = c("the Great", "the Younger", "the Dung-named", "the Little", "the Great", "the Butcher", "the Great", "the Slit-nosed"),
-    responders = 30:37,
-    sampleSize = 100:107)
-  expected_studyData = data.frame(
-      study = c("Constantine", "Leo", "Justinian"),
-      age = rep(99, 3))
+  expected_data <- list(
+    armData = data.frame(
+      study=c(rep("Constantine",3), rep("Leo",3), rep("Justinian",2)),
+      treatment=c("the Great", "the Younger", "the Dung-named", "the Little", "the Great", "the Butcher", "the Great", "the Slit-nosed"),
+      responders=30:37,
+      sampleSize=100:107),
+    studyData = data.frame(
+      study=c("Constantine", "Leo", "Justinian"),
+      age=rep(99,3))
+  )
   
-  expect_equal(gemtc_data$armData, expected_armData)
-  expect_equal(gemtc_data$studyData, expected_studyData)
+  expect_equal(gemtc_data, expected_data)
 })
 
 test_that("PrepDataGemtc() gives correct data for wide continuous", {
@@ -39,18 +41,19 @@ test_that("PrepDataGemtc() gives correct data for wide continuous", {
   data <- read.csv("Cont_wide_cov.csv")
   gemtc_data <- PrepDataGemtc(data, "Continuous", "covar.age", "age")
   
-  expected_armData <- data.frame(
-    study = c(rep("Constantine", 3), rep("Leo", 3), rep("Justinian", 2)),
-    treatment = c("the Great", "the Younger", "the Dung-named", "the Little", "the Great", "the Butcher", "the Great", "the Slit-nosed"),
-    mean = c(-1, -1.1, -1.2, -1.3, -1.4, -1.5, -1.6, -1.7),
-    std.dev = c(11.1, 12.2, 13.3, 14.4, 15.5, 16.6, 17.7, 18.8),
-    sampleSize = 30:37)
-  expected_studyData = data.frame(
-      study = c("Constantine", "Leo", "Justinian"),
-      age = rep(99, 3))
+  expected_data <- list(
+    armData = data.frame(
+      study=c(rep("Constantine",3), rep("Leo",3), rep("Justinian",2)),
+      treatment=c("the Great", "the Younger", "the Dung-named", "the Little", "the Great", "the Butcher", "the Great", "the Slit-nosed"),
+      mean=c(-1, -1.1, -1.2, -1.3, -1.4, -1.5, -1.6, -1.7),
+      std.dev=c(11.1, 12.2, 13.3, 14.4, 15.5, 16.6, 17.7, 18.8),
+      sampleSize=30:37),
+    studyData = data.frame(
+      study=c("Constantine", "Leo", "Justinian"),
+      age=rep(99,3))
+  )
   
-  expect_equal(gemtc_data$armData, expected_armData)
-  expect_equal(gemtc_data$studyData, expected_studyData)
+  expect_equal(gemtc_data, expected_data)
 })
 
 test_that("PrepDataGemtc() gives correct data for long continuous", {
@@ -58,16 +61,17 @@ test_that("PrepDataGemtc() gives correct data for long continuous", {
   data <- read.csv("Cont_long_cov.csv")
   gemtc_data <- PrepDataGemtc(data, "Continuous", "covar.age", "age")
   
-  expected_armData <- data.frame(
-    study = c(rep("Constantine", 3), rep("Leo", 3), rep("Justinian", 2)),
-    treatment = c("the Great", "the Younger", "the Dung-named", "the Little", "the Great", "the Butcher", "the Great", "the Slit-nosed"),
-    mean = c(-1, -1.1, -1.2, -1.3, -1.4, -1.5, -1.6, -1.7),
-    std.dev = c(11.1, 12.2, 13.3, 14.4, 15.5, 16.6, 17.7, 18.8),
-    sampleSize = 30:37)
-  expected_studyData = data.frame(
-      study = c("Constantine", "Leo", "Justinian"),
-      age = rep(99, 3))
+  expected_data <- list(
+    armData = data.frame(
+      study=c(rep("Constantine",3), rep("Leo",3), rep("Justinian",2)),
+      treatment=c("the Great", "the Younger", "the Dung-named", "the Little", "the Great", "the Butcher", "the Great", "the Slit-nosed"),
+      mean=c(-1, -1.1, -1.2, -1.3, -1.4, -1.5, -1.6, -1.7),
+      std.dev=c(11.1, 12.2, 13.3, 14.4, 15.5, 16.6, 17.7, 18.8),
+      sampleSize=30:37),
+    studyData = data.frame(
+      study=c("Constantine", "Leo", "Justinian"),
+      age=rep(99,3))
+  )
   
-  expect_equal(gemtc_data$armData, expected_armData)
-  expect_equal(gemtc_data$studyData, expected_studyData)
+  expect_equal(gemtc_data, expected_data)
 })
