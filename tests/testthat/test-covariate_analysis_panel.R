06157a02317e4e405d2250de3da071c1ca7a6b56
JanionNevill <janion.nevill@leicester.ac.uk>
2023-10-27 12:31:34 +0100

    Meta-regression tab now always visible and has a bit more structure

--- tests/testthat/test-covariate_analysis_panel.R
+++ tests/testthat/test-covariate_analysis_panel.R
@@ -3,7 +3,7 @@ test_that("Covariate title NA when not available", {
   df <- read.csv("Cont_long.csv") %>%
     CleanData()
   testServer(
-    meta_regression_tab_server,
+    covariate_analysis_panel_server,
     args = list(all_data = function() { df }),
     {
       expect_true(is.na(covariate_title()))
@@ -15,7 +15,7 @@ test_that("Covariate title extracted from data when available", {
   df <- read.csv("Cont_long_continuous_cov.csv") %>%
     CleanData()
   testServer(
-    meta_regression_tab_server,
+    covariate_analysis_panel_server,
     args = list(all_data = function() { df }),
     {
       expect_equal(covariate_title(), "covar.age")
@@ -26,7 +26,7 @@ test_that("Covariate name NA when not available", {
   df <- read.csv("Cont_long.csv") %>%
     CleanData()
   testServer(
-    meta_regression_tab_server,
+    covariate_analysis_panel_server,
     args = list(all_data = function() { df }),
     {
       expect_true(is.na(covariate_name()))
@@ -38,37 +38,11 @@ test_that("Covariate name extracted from data when available", {
   df <- read.csv("Cont_long_continuous_cov.csv") %>%
     CleanData()
   testServer(
-    meta_regression_tab_server,
+    covariate_analysis_panel_server,
     args = list(all_data = function() { df }),
     {
       expect_equal(covariate_name(), "age")
       expect_equal(output$subtitle, "Covariate: age")
-    }
-  )
-})
-
-test_that("Covariate presence status passed back to module parent when not available", {
-  df <- read.csv("Cont_long.csv") %>%
-    CleanData()
-  testServer(
-    meta_regression_tab_server,
-    args = list(all_data = function() { df }),
-    {
-      expect_equal(length(session$returned), 1)
-      expect_true(session$returned())
-    }
-  )
-})
-
-test_that("Covariate presence status passed back to module parent when available", {
-  df <- read.csv("Cont_long_continuous_cov.csv") %>%
-    CleanData()
-  testServer(
-    meta_regression_tab_server,
-    args = list(all_data = function() { df }),
-    {
-      expect_equal(length(session$returned), 1)
-      expect_false(session$returned())
     }
   )
 })
