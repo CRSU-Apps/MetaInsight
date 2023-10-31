5c2dbed1882a68a850cee7c6d3a5d06b601db02f
Clareece Nevill <clareece.nevill@gmail.com>
2023-10-31 11:09:19 +0000

    Improved formatting, regex, and tests as per code review

--- R/gemtc_analysis.R
+++ R/gemtc_analysis.R
@@ -6,9 +6,8 @@
 #' @param data Uploaded data from the user (or in-built datasets)
 #' @param outcome_type Indicator of whether data is binary or continuous
 #' @param covariate Chosen covariate name as per uploaded data
-#' @param cov_friendly Friendly version of chosen covariate
-#' @return list containing two dataframes: 1 of core data, another for covariate data
-
+#' @param cov_friendly Friendly name of chosen covariate
+#' @return list containing two dataframes: armData containing the core data; studyData containing covariate data
 PrepDataGemtc <- function(data, outcome_type, covariate, cov_friendly){
   # ensure data is in long format
   if (FindDataShape(data) == "wide") {
@@ -18,22 +17,22 @@ PrepDataGemtc <- function(data, outcome_type, covariate, cov_friendly){
   }
   # specify arm level data
   if (outcome_type == "Continuous") {
-    armData <- data.frame(study=long_data$Study,
-                          treatment=long_data$T,
-                          mean=long_data$Mean,
-                          std.dev=long_data$SD,
-                          sampleSize=long_data$N)
+    armData <- data.frame(study = long_data$Study,
+                          treatment = long_data$T,
+                          mean = long_data$Mean,
+                          std.dev = long_data$SD,
+                          sampleSize = long_data$N)
   } else {
-    armData <- data.frame(study=long_data$Study,
-                          treatment=long_data$T,
-                          responders=long_data$R,
-                          sampleSize=long_data$N)
+    armData <- data.frame(study = long_data$Study,
+                          treatment = long_data$T,
+                          responders = long_data$R,
+                          sampleSize = long_data$N)
   }
   # specify study level data
-  studyData <- unique(data.frame(study=long_data$Study,
+  studyData <- unique(data.frame(study = long_data$Study,
                                  cov_name = long_data[,covariate]))
   names(studyData)[2] <- cov_friendly
   rownames(studyData) <- NULL
   
-  return(list(armData=armData,studyData=studyData))
+  return(list(armData = armData, studyData = studyData))
 }
