ed99a5a6ec8962924d96a16483b62abc7fcf93dc
Tom Morris <tommorris168@gmail.com>
2023-11-15 10:10:57 +0000

    Added test_that file, also various additional changes to BnmaAnalysis.

--- R/BnmaAnalysis.R
+++ R/BnmaAnalysis.R
@@ -1,26 +1,36 @@
 #' Takes MetaInsight data and converts it into BNMA format
 #'
-#' @param BRdata A data frame of data in MetaInsight format
-#' @param dataFormat "Long" or "Wide"
-#' @param outcomeType "Continuous" or "Binomial"
-#' @param ref An element of 'BRdata$T', the reference treatment
-#' @return A list with elements 'ArmLevel' and 'Treat.order'
-#'          'ArmLevel' is a data frame containing 'Study', 'Treat', 'N', 'Outcomes',
-#'          and (for outcomeType="continuous") 'SD'
-FormatForBnma <- function(BRdata, dataFormat, outcomeType, ref){
-  if(dataFormat == "Wide"){
-    BRdata2 <- as.data.frame(WideToLong(BRdata, ConBi=outcomeType))
-  } else if(dataFormat == "Long"){
-    BRdata2 <- BRdata
-  }
+#' @param br_data A data frame of data in MetaInsight format
+#' @param treatment_ids Data frame containing treatment IDs and names in columns named 'Number' and 'Label' respectively
+#' @param outcome_type "Continuous" or "Binary"
+#' @param ref An element of 'br_data$T', the reference treatment
+#' @return List:
+#'  - 'ArmLevel' = Data frame containing 'Study', 'Treat', 'N', 'Outcomes', and (for outcome_type="Continuous") 'SD'
+#'  - 'Treat.order' = Vector of (unique) treatments, with the reference treatment first
+FormatForBnma <- function(br_data, treatment_ids, outcome_type, ref){
+  if(FindDataShape(br_data) == "wide"){
+    br_data2 <- as.data.frame(WideToLong(br_data, outcome_type=outcome_type))
+  } else if(FindDataShape(br_data) == "long"){
+    br_data2 <- br_data
+  } else {paste0("data_format has to be 'wide' or 'long'")}
+  
+  #Use wrangled treatment names
+  br_data3 <- br_data2
+  br_data3$T <- treatment_ids$Label[match(br_data2$T, treatment_ids$Number)]
+  
   #Treatment order (put 'ref' first)
-  Treat.order <- c(ref, unique(BRdata2$T)[-which(unique(BRdata2$T) == ref)])
+  Treat.order <- VectorWithItemFirst(vector = unique(br_data3$T), first_item = ref)
+  
   #Arm-level data
-  if(outcomeType == "Binomial"){
-    ArmLevel <- dplyr::rename(BRdata2, "Treat"="T", "Outcomes"="R")
-  } else if(outcomeType == "Continuous"){
-    ArmLevel <- dplyr::rename(BRdata2, "Treat"="T", "Outcomes"="Mean")
-  }
+  if(outcome_type == "Binary"){
+    ArmLevel <- dplyr::rename(br_data3, "Treat"="T", "Outcomes"="R")
+    ArmLevel <- dplyr::select(ArmLevel, c("Study", "Treat", "Outcomes", "N"))
+  } else if(outcome_type == "Continuous"){
+    ArmLevel <- dplyr::rename(br_data3, "Treat"="T", "Outcomes"="Mean")
+    ArmLevel <- dplyr::select(ArmLevel, c("Study", "Treat", "Outcomes", "SD", "N"))
+  } else {paste0("outcome_type has to be 'Continuous' or 'Binary'")}
+  
+  
   return(list(ArmLevel=ArmLevel, Treat.order=Treat.order))
 }
 
@@ -29,40 +39,44 @@ FormatForBnma <- function(BRdata, dataFormat, outcomeType, ref){
 
 #' Fits the baseline risk meta-regression model in BNMA
 #'
-#' @param BRdata A list of data in the format produced by the 'FormatForBnma' function.
-#' @param outcomeType "Continuous" or "Binomial".
-#' @param effectsType "fixed" or "random".
-#' @param covParameters "common", "exchangable", or "independent".
-#' @param seeds Vector of seeds, which also determines the number of chains. Defaults to 123:125.
+#' @param br_data A list of data in the format produced by the 'FormatForBnma' function.
+#' @param outcome_type "Continuous" or "Binary".
+#' @param effects_type "fixed" or "random".
+#' @param cov_parameters "common", "exchangable", or "independent".
+#' @param seed Seed. Defaults to 123.
 #' @return Output from bnma::network.run.
-BaselineRiskRegression <- function(BRdata, outcomeType, effectsType, covParameters, seeds=123:125){
-  #Put the seeds in the required format
+BaselineRiskRegression <- function(br_data, outcome_type, effects_type, cov_parameters, seed=123){
+  #Select random seeds for the four chains based on 'seed'
+  set.seed(seed)
+  seeds <- sample.int(4, n = .Machine$integer.max)
+
+  #Put the seeds in the required format for passing to bnma::network.run
   rng_inits <- list()
   for(i in 1:length(seeds)){
     rng_inits[[i]] <- list(.RNG.name = "base::Wichmann-Hill", .RNG.seed = seeds[i])
   }
-  
-  if(outcomeType == "Binomial"){
-    network <- with(BRdata, bnma::network.data(Outcomes = ArmLevel$Outcomes,
-                                                 Study = ArmLevel$Study,
-                                                 Treat = ArmLevel$Treat,
-                                                 N = ArmLevel$N,                                          
-                                                 response = "binomial",
-                                                 Treat.order = Treat.order,
-                                                 type = effectsType,
-                                                 baseline = covParameters,
-                                                 baseline.risk = "independent"))
+
+  if(outcome_type == "Binary"){
+    network <- with(br_data, bnma::network.data(Outcomes = ArmLevel$Outcomes,
+                                                Study = ArmLevel$Study,
+                                                Treat = ArmLevel$Treat,
+                                                N = ArmLevel$N,
+                                                response = "binomial",
+                                                Treat.order = Treat.order,
+                                                type = effects_type,
+                                                baseline = cov_parameters,
+                                                baseline.risk = "independent"))
   }
-  if(outcomeType == "Continuous"){
-    network <- with(BRdata, bnma::network.data(Outcomes = ArmLevel$Outcomes,
-                                                 Study = ArmLevel$Study,                                           
-                                                 Treat = ArmLevel$Treat,
-                                                 response = "normal",
-                                                 SE = ArmLevel$SD / sqrt(ArmLevel$N),
-                                                 Treat.order = Treat.order,
-                                                 type = effectsType,
-                                                 baseline = covParameters,
-                                                 baseline.risk = "independent"))
+  if(outcome_type == "Continuous"){
+    network <- with(br_data, bnma::network.data(Outcomes = ArmLevel$Outcomes,
+                                                Study = ArmLevel$Study,
+                                                Treat = ArmLevel$Treat,
+                                                response = "normal",
+                                                SE = ArmLevel$SD / sqrt(ArmLevel$N),
+                                                Treat.order = Treat.order,
+                                                type = effects_type,
+                                                baseline = cov_parameters,
+                                                baseline.risk = "independent"))
   }
   return(bnma::network.run(network,
                            n.run=10000,
