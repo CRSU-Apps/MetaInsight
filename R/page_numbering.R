ee45e62a816ea19cdc7a043869bed7b7443d716c
JanionNevill <janion.nevill@leicester.ac.uk>
2024-02-14 09:29:16 +0000

    Documented class

--- R/page_numbering.R
+++ R/page_numbering.R
@@ -1,17 +1,33 @@
 
+#' @title Page Numbering
+#' 
+#' @description
+#' Class for numbering pages in a logical manner.
 PageNumbering <- R6::R6Class(
   classname = "Page Numbering",
   public = list(
+    #' @description
+    #' Create a new instance of this [R6][R6::R6Class] class.
     initialize = function() {
       private$root <- data.tree::Node$new("")
       private$current <- private$root
     },
+    #' Add a child at the current level. The pages will be numbered with:
+    #' - Hindu-Arabic numerals (eg. 1, 2, 3, 4)
+    #' - Latin alphabet letters (eg. a, b, c, d)
+    #' - Roman numerals (eg. i, ii, iii, iv)
+    #' These will be arranged in the repeating format "1b iii-...".
+    #'
+    #' @return Formatted page number
     AddChild = function() {
       child <- private$current$AddChild(
         private$GetNextPageNumber()
       )
       return(private$BuildPageNumber(child))
     },
+    #' Create a sub-level within the current numbering level.
+    #'
+    #' @return self
     DiveLevel = function() {
       current <- private$current
       if (length(current$children) == 0) {
@@ -20,6 +36,9 @@ PageNumbering <- R6::R6Class(
       private$current <- current$children[[length(current$children)]]
       invisible(self)
     },
+    #' Move up out of the current sub-level.
+    #'
+    #' @return self
     FloatLevel = function() {
       if (is.null(private$current$parent)) {
         stop("Cannot float level above root")
@@ -29,9 +48,16 @@ PageNumbering <- R6::R6Class(
     }
   ),
   private = list(
+    #' @field root
+    #' The top-level for numbering the entire app.
     root = NULL,
+    #' @field current
+    #' The current numbering level.
     current = NULL,
     
+    #' Find the next identifier at the current level.
+    #'
+    #' @return String of the next identifier at this level.
     GetNextPageNumber = function() {
       # Numbers
       if (private$current$level %% 3 == 1) {
@@ -62,6 +88,11 @@ PageNumbering <- R6::R6Class(
       }
     },
     
+    #' Build the full page number for a given node. This will combine all parents in the repeating format "1b iii-...".
+    #'
+    #' @param node Node for which to build page number.
+    #'
+    #' @return Full formatted page number.
     BuildPageNumber = function(node) {
       name = node$name
       
