c0291b3ddfde0dc239d96a7306c88d6f28393a69
Tom Morris <tommorris168@gmail.com>
2024-05-03 16:36:24 +0100

    Added baseline risk novel plot.

---
+++ R/meta_regression/baseline_risk_value_panel.R
@@ -0,0 +1,40 @@
+.CreateInlineBlock <- function(..., style = NULL) {
+  return(
+    div(
+      ...,
+      style = paste0("display: inline-block; ", style)
+    )
+  )
+}
+
+#' Create the baseline risk value panel.
+#'
+#' @param id ID of the module.
+#' @return Div containing the module UI.
+baseline_risk_value_panel_ui <- function(id) {
+  ns <- NS(id)
+  div(
+    title = "This value is used by all baseline risk analysis output",
+    style = "padding-left: 5px;",
+    .CreateInlineBlock(
+      h4("Covariate value:", textOutput(ns("mean_covariate_value"), inline = TRUE), tags$i(class="fa-regular fa-circle-question")),
+      style = "padding-right: 5pt;"
+    )
+  )
+}
+
+#' Create the covariate value panel server.
+#'
+#' @param id ID of the module.
+#' @param reference_outcomes Reactive containing the observed outcomes in the reference arm.
+baseline_risk_value_panel_server <- function(id, reference_outcomes) {
+  shiny::moduleServer(id, function(input, output, session) {
+    
+    output$mean_covariate_value <- reactive(
+      round(mean(reference_outcomes, na.rm = TRUE),
+            digits = 3)
+      )
+    outputOptions(x = output, name = "mean_covariate_value", suspendWhenHidden = FALSE)
+    
+  })
+}
--- R/meta_regression/covariate_analysis.R
+++ R/meta_regression/covariate_analysis.R
@@ -5,12 +5,16 @@
 #' @param data Data frame from which to find covariate ranges
 #' @param treatment_ids data frame containing treatment names ("Label") and IDs ("Number")
 #' @param reference Name of reference treatment.
-#' @param covariate_title Title of covariate column in data.
+#' @param covariate_title Title of covariate column in data. Only required when @param baseline_risk == FALSE.
+#' @param baseline_risk TRUE if the covariate is baseline risk. Defaults to FALSE.
+#' @param outcome_type "Binary" or "Continuous". Only required when @param baseline_risk == TRUE. Defaults to NULL.
+#' @param model Model created by bnma::network.run(). Only required when @param baseline_risk == TRUE. Defaults to NULL.
 #'
 #' @return The lowest and highest covariate values of relevant studies. This is structured as a list containing 2 items:
 #' - "min" a named vector of the lowest values, where the names are the treatment names.
 #' - "max" a named vector of the highest values, where the names are the treatment names.
-FindCovariateRanges <- function(data, treatment_ids, reference, covariate_title) {
+FindCovariateRanges <- function(data, treatment_ids, reference, covariate_title, baseline_risk = FALSE, outcome_type = NULL, model = NULL) {
+  
   studies <- unique(data$Study)
   
   study_treatments <- sapply(
@@ -42,7 +46,17 @@ FindCovariateRanges <- function(data, treatment_ids, reference, covariate_title)
     study_treatments <- temp_matrix
   }
   
+  colnames(study_treatments) <- studies
+  
   non_reference_treatment_names <- treatment_ids$Label[treatment_ids$Label != reference]
+  
+  if (baseline_risk) {
+    baseline_risk_covariate <- GetReferenceOutcome(data = data,
+                                                   treatment_ids = treatment_ids,
+                                                   outcome_type = outcome_type,
+                                                   observed = "Imputed",
+                                                   model = model)
+  }
   
   treatment_min <- c()
   treatment_max <- c()
@@ -52,7 +66,11 @@ FindCovariateRanges <- function(data, treatment_ids, reference, covariate_title)
     for (study in studies) {
       
       if (treatment_name %in% study_treatments[, study] && reference %in% study_treatments[, study]) {
-        covariate_value <- data[[covariate_title]][data$Study == study][1]
+        if (!baseline_risk) {
+          covariate_value <- data[[covariate_title]][data$Study == study][1]
+        } else {
+          covariate_value <- baseline_risk_covariate[study]
+        }
         
         if (is.na(min) || min > covariate_value) {
           min <- covariate_value
@@ -63,8 +81,8 @@ FindCovariateRanges <- function(data, treatment_ids, reference, covariate_title)
         }
       }
     }
-    treatment_min[[treatment_name]] <- min
-    treatment_max[[treatment_name]] <- max
+    treatment_min[[treatment_name]] <- unname(min)
+    treatment_max[[treatment_name]] <- unname(max)
   }
   
   return(
