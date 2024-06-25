eeadc93ad5c8c8d9346f9b816d5524c7149f7f87
Tom Morris <tommorris168@gmail.com>
2024-06-25 15:17:37 +0100

    Updated tests.

--- tests/testthat/test-DataInputPanel.R
+++ tests/testthat/test-DataInputPanel.R
@@ -21,7 +21,7 @@ test_that("Treatments extracted from default continuous file", {
     session$setInputs(metaoutcome = 'Continuous')
     
     treatment_df <- data.frame(Number = seq(4),
-                               Label = c('Glucocorticoids','Placebo','Ketamine','Gabapentinoids'))
+                               Label = c('Placebo','Glucocorticoids','Ketamine','Gabapentinoids'))
     expect_equal(treatment_list(), treatment_df)
   })
 })
@@ -30,9 +30,9 @@ test_that("Treatments reordered from default continuous file when reference sele
   testServer(data_input_panel_server, {
     session$setInputs(metaoutcome = 'Continuous')
     
-    session$setInputs(reference_treatment = 'Placebo')
+    session$setInputs(reference_treatment = 'Glucocorticoids')
     treatment_df <- data.frame(Number = seq(4),
-                               Label = c('Placebo','Glucocorticoids','Ketamine','Gabapentinoids'))
+                               Label = c('Glucocorticoids','Placebo','Ketamine','Gabapentinoids'))
 
     expect_equal(treatment_list(), treatment_df)
   })
@@ -48,16 +48,17 @@ test_that("Data extracted from default continuous file", {
                  label = nrow(data()))
     expect_equal(data()$Study, rep(1:45, each = 2),
                  label = format_vector_to_string(data()$Study))
-    expect_equal(data()$T, c(rep(c("Glucocorticoids", "Placebo"), times = 4),
-                             rep(c("Ketamine", "Placebo"), times = 18),
-                             rep(c("Gabapentinoids", "Placebo"), times = 23)
+    expect_equal(data()$T, c(rep(c("Placebo", "Glucocorticoids"), times = 4),
+                             rep(c("Placebo", "Ketamine"), times = 18),
+                             rep(c("Placebo", "Gabapentinoids"), times = 23)
                              ),
                  label = format_vector_to_string(data()$T))
-    expect_equal(data()$N, c(62, 50,	92,	45,	78,	88,	46,	47,	43,	52,	84,	84,	33,	34,	34,	35,	73,	66,	18,	19,	8, 6, 31,	47,	25,	24,	15,	17,	33,	35,	49,	42,	118, 62, 22, 22, 8,	7, 55, 25, 39, 38, 31, 30, 21, 10, 65, 53, 20, 20, 27, 35, 23, 22, 2,	4, 60, 30, 50, 50, 20, 20, 30, 29, 17,	24,	50,	49,	83,	28,	100, 100, 22,	24,	52,	48,	62,	31,	18,	20,	16,	18,	46,	46,	27,	27,	70,	35,	75,	76),
+    expect_equal(data()$N, c(50, 62, 45, 92, 88, 78, 47, 46, 52, 43, 84, 84, 34, 33, 35, 34, 66, 73, 19, 18, 6, 8, 47, 31, 24, 25, 17, 15, 35, 33, 42, 49, 62, 118, 22, 22, 7, 8, 25, 55, 38, 39, 30, 31, 10, 21, 53, 65, 20, 20, 35, 27, 22, 23, 4, 2, 30, 60, 50, 50, 20, 20, 29, 30,
+24, 17, 49, 50, 28, 83, 100, 100, 24, 22, 48, 52, 31, 62, 20, 18, 18, 16, 46, 46, 27, 27, 35, 70, 76, 75),
                  label = format_vector_to_string(data()$N))
-    expect_equal(data()$Mean, c(1.9, 2.2,	2, 2,	1.4, 1.4,	2.6, 2.9,	3, 5.4,	1, 1,	0, 0,	0, 0,	4, 4,	1.1, 0.3,	2, 0.5,	1.4, 1.3,	0, 0, 0.3, 1.5,	0.7, 1,	4.3, 4,	2, 2,	0, 1.5,	2.4, 3.4,	0.5, 1,	0.2, 0.1,	0.8, 0.8,	3.1, 3.4,	0, 0,	0.2, 0.6,	0.5, 0.6,	2.6, 2.2,	3, 2.5,	0.5,	1.2, 2.1,	2.2, 0.1,	0.2, 1,	2, 0.9,	1.1, 2,	3, 1.8,	2, 2.5,	3.4, 1.5,	1.9, 0,	0, 2,	3, 1.1,	1.5, 1,	0.5, 0,	0, 1.3,	1.5, 0.4,	0.03, 1.4,	1.2),
+    expect_equal(data()$Mean, c(2.2, 1.9, 2, 2, 1.4, 1.4, 2.9, 2.6, 5.4, 3, 1, 1, 0, 0, 0, 0, 4, 4, 0.3, 1.1, 0.5, 2, 1.3, 1.4, 0, 0, 1.5, .3, 1, 0.7, 4, 4.3, 2, 2, 1.5, 0, 3.4, 2.4, 1, 0.5, 0.1, 0.2, 0.8, 0.8, 3.4, 3.1, 0, 0, 0.6, 0.2, 0.6, 0.5, 2.2, 2.6, 2.5, 3, 1.2, 0.5, 2.2,2.1, 0.2, 0.1, 2, 1, 1.1, 0.9, 3, 2, 2, 1.8, 3.4, 2.5, 1.9, 1.5, 0, 0, 3, 2, 1.5, 1.1, 0.5, 1, 0, 0, 1.5, 1.3, 0.03, 0.4, 1.2, 1.4),
                  label = format_vector_to_string(data()$Mean))
-    expect_equal(data()$SD, c(2.4,	2.5,	2.8,	3.7,	1.7,	1.6,	2.4,	2.5,	3.5,	3.6,	1.5,	1.5,	0.7,	0.2,	0.3,	0.1,	2.2,	3.3,	2.1,	0.7,	1.7,	3.7,	1.4,	1.5,	0.7,	1.9,	3,	4.4,	1.4,	1.7,	2.5,	2.1,	1.5,	1.5,	1.5,	2.2,	1.1,	3,	1.7,	1.8,	0.7,	0.7,	0.9,	1.1,	1.7,	1.6,	0.7,	0.7,	0.4,	0.9,	0.9,	0.1,	1.7,	1.4,	0.8,	1,	0.9,	1.5,	1.2,	0.9,	0.1,	0.1,	0.6,	1.5,	1.2,	1.5,	1.5,	1.9,	2,	2.5,	1.2,	1.4,	1.9,	2,	2.1,	0.7,	0.5,	0.8,	1.6,	2,	1.5,	1.5,	0.7,	0.7,	2.2,	2.8,	0.9,	0.2,	1.7,	1.7),
+    expect_equal(data()$SD, c(2.5, 2.4, 3.7, 2.8, 1.6, 1.7, 2.5, 2.4, 3.6, 3.5, 1.5, 1.5, 0.2, 0.7, 0.1, 0.3, 3.3, 2.2, 0.7, 2.1, 3.7, 1.7, 1.5, 1.4, 1.9, 0.7, 4.4, 3, 1.7, 1.4, 2.1, 2.5, 1.5, 1.5, 2.2, 1.5, 3, 1.1, 1.8, 1.7, 0.7, 0.7, 1.1, 0.9, 1.6, 1.7, 0.7, 0.7, 0.9, 0.4, 0.1,0.9, 1.4, 1.7, 1, 0.8, 1.5, 0.9, 0.9, 1.2, 0.1, 0.1, 1.5, 0.6, 1.5, 1.2, 1.9, 1.5, 2.5, 2, 1.4, 1.2, 2, 1.9, 0.7, 2.1, 0.8, 0.5, 2, 1.6, 1.5, 1.5, 0.7, 0.7, 2.8, 2.2, 0.2, 0.9, 1.7, 1.7),
                  label = format_vector_to_string(data()$SD))
   })
 })
