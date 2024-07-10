9e651947af5700c9735a093c156e099592f10f85
JanionNevill <janion.nevill@leicester.ac.uk>
2024-07-10 08:54:04 +0100

    Removed comments, updated headers, updated notation

--- R/meta_regression/add_remove_options_panel.R
+++ R/meta_regression/add_remove_options_panel.R
@@ -1,5 +1,5 @@
 
-#' Create the regression plot panel.
+#' Create the panel for adding and removing treatments.
 #'
 #' @param id ID of the module.
 #' @return Div containing the module UI.
@@ -9,7 +9,7 @@ add_remove_panel_ui <- function(id) {
   div(
     # Add treatments
     selectInput(
-      inputId = ns("add_treatment_dropdown"),
+      inputId = ns("add_comparator_dropdown"),
       label = .AddRegressionOptionTooltip(
         tags$html("Add treatment", tags$i(class="fa-regular fa-circle-question")),
         tooltip = "All treatments are compared to the reference treatment"
@@ -17,7 +17,7 @@ add_remove_panel_ui <- function(id) {
       choices = c(),
       selectize = FALSE
     ),
-    actionButton(inputId = ns("add_treatment_btn"), label = "Add to plot"),
+    actionButton(inputId = ns("add_comparator_btn"), label = "Add to plot"),
     div(
       style = "float: right;",
       actionButton(inputId = ns("add_all_btn"), label = "Add all")
@@ -25,8 +25,8 @@ add_remove_panel_ui <- function(id) {
     br(),
     br(),
     # Remove treatments
-    selectInput(inputId = ns("remove_treatment_dropdown"), label = "Remove treatment", choices = c(), selectize = FALSE),
-    actionButton(inputId = ns("remove_treatment_btn"), label = "Remove"),
+    selectInput(inputId = ns("remove_comparator_dropdown"), label = "Remove treatment", choices = c(), selectize = FALSE),
+    actionButton(inputId = ns("remove_comparator_btn"), label = "Remove"),
     div(
       style = "float: right;",
       actionButton(inputId = ns("remove_all_btn"), label = "Remove all")
@@ -34,30 +34,26 @@ add_remove_panel_ui <- function(id) {
   )
 }
 
-#' Create the regression plot server.
+#' Create the server for adding and removing treatments.
 #'
 #' @param id ID of the module.
-#' @param data Reactive containing study data including covariate columns, in wide or long format.
-#' @param covariate_title Reactive containing title of the covariate column in the data.
-#' @param covariate_name Friendly name of chosen covariate.
-#' @param model_output Reactive containing model results found by calling `CovariateModelOutput()` or `BaselineRiskModelOutput()`.
-#' @param treatment_df Reactive containing data frame containing treatment IDs (Number), sanitised names (Label), and original names (RawLabel).
-#' @param outcome_type Reactive containing meta analysis outcome: "Continuous" or "Binary".
-#' @param outcome_measure Reactive type of outcome (OR, RR, RD, MD or SD).
-#' @param package package used to create the model. Either "gemtc" (default) or "bnma".
-add_remove_panel_server <- function(id, treatment_names, reference_name) {
+#' @param comparator_names Reactive containing vector of all treatment names.
+#' @param reference_name Reactive containing reference treatment name.
+#'
+#' @return Reactive containing added treatment names.
+add_remove_panel_server <- function(id, comparator_names, reference_name) {
   shiny::moduleServer(id, function(input, output, session) {
     
-    added_treatments <- reactiveVal(c())
+    added_comparators <- reactiveVal(c())
     # These reactive values are here to prevent overly chatty reactives from repeatedly clearing the
     # treatment drop-down boxes when not appropriate
-    current_treatment_names <- reactiveVal()
+    current_comparator_names <- reactiveVal()
     current_reference_name <- reactiveVal()
     
     # Update treatment list only if changed
     observe({
-      if (!setequal(current_treatment_names(), treatment_names())) {
-        current_treatment_names(treatment_names())
+      if (!setequal(current_comparator_names(), comparator_names())) {
+        current_comparator_names(comparator_names())
       }
     })
     
@@ -70,53 +66,51 @@ add_remove_panel_server <- function(id, treatment_names, reference_name) {
     
     # Clear added treatments when treatment list or reference change
     observe({
-      added_treatments(character())
+      added_comparators(character())
     }) |>
-      bindEvent(current_reference_name(), current_treatment_names())
+      bindEvent(current_reference_name(), current_comparator_names())
     
     available_to_add <- reactive({
-      return(treatment_names()[(treatment_names() != reference_name()) & !(treatment_names() %in% added_treatments())])
+      return(comparator_names()[(comparator_names() != reference_name()) & !(comparator_names() %in% added_comparators())])
     })
     
     # Add treatment on button click
     observe({
-      added = unique(c(added_treatments(), input$add_treatment_dropdown))
-      added_treatments(added)
+      added = unique(c(added_comparators(), input$add_comparator_dropdown))
+      added_comparators(added)
     }) |>
-      bindEvent(input$add_treatment_btn)
+      bindEvent(input$add_comparator_btn)
     
     # Add all treatments on button click
     observe({
-      added = unique(c(added_treatments(), available_to_add()))
-      added_treatments(added)
+      added = unique(c(added_comparators(), available_to_add()))
+      added_comparators(added)
     }) |>
       bindEvent(input$add_all_btn)
     
     # Remove treatment on button click
     observe({
-      added = added_treatments()[added_treatments() != input$remove_treatment_dropdown]
-      added_treatments(added)
+      added = added_comparators()[added_comparators() != input$remove_comparator_dropdown]
+      added_comparators(added)
     }) |>
-      bindEvent(input$remove_treatment_btn, ignoreNULL = FALSE)
+      bindEvent(input$remove_comparator_btn, ignoreNULL = FALSE)
     
     # Remove all treatments on button click
     observe({
-      added_treatments(character())
+      added_comparators(character())
     }) |>
       bindEvent(input$remove_all_btn, ignoreNULL = FALSE)
     
     # Update inputs on added treatments change
     observe({
-      updateSelectInput(inputId = "add_treatment_dropdown", choices = available_to_add())
+      updateSelectInput(inputId = "add_comparator_dropdown", choices = available_to_add())
     })
     
     # Update inputs on added treatments change
     observe(priority = -1000, {
-      # print("Updating input")
-      # print(added_treatments())
-      updateSelectInput(inputId = "remove_treatment_dropdown", choices = added_treatments())
+      updateSelectInput(inputId = "remove_comparator_dropdown", choices = added_comparators())
     })
     
-    return(added_treatments)
+    return(added_comparators)
   })
 }
