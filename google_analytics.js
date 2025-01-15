b6395c5ba41ea6a856da7995db5c30f35946c260
JanionNevill <janion.nevill@leicester.ac.uk>
2025-01-15 10:14:44 +0000

    Copied correct javascript from metaCNMA

--- google_analytics.js
+++ google_analytics.js
@@ -1,29 +1,17 @@
-(function(i,s,o,g,r,a,m){
-  i['GoogleAnalyticsObject']=r;
-  i[r]=i[r] || 
-  function(){
-    (i[r].q=i[r].q||[]).push(arguments);
-  },i[r].l=1*new Date();
-  a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];
-  a.async=1;
-  a.src=g;
-  m.parentNode.insertBefore(a,m);
-})(window,document,'script',
-  'https://www.google-analytics.com/analytics.js','ga');
+// Import the Google Tag JS
+var js = document.createElement("script");
+js.type = "text/javascript";
+js.src = "https://www.googletagmanager.com/gtag/js?id=<<GOOGLE_ANALYTICS_ID>>";
+document.body.appendChild(js);
 
-ga('create', '<<GOOGLE_ANALYTICS_ID>>', 'auto');
-ga('send', 'pageview');
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', '<<GOOGLE_ANALYTICS_ID>>');
 
 // Event Tracking Code
-
-$(document).on('shiny:inputchanged', function(event) {
-  if(event.name == 'data' ){
-    ga('send', 'event', 'input',
-      'Upload data', event.name, event.value);
-  }
-});
-
-
-
-
+Shiny.addCustomMessageHandler('add-event', function(msg){
+  gtag('event', msg.value, {
+    'event_value': msg.value
+  });
+})
