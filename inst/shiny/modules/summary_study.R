9883891bf8e65c7072dd96f849da0ffdce4bda0a
simon-smart88 <simon.smart@cantab.net>
2025-02-10 17:06:47 +0000

    tidy ids, merge plot_height function

--- inst/shiny/modules/summary_study.R
+++ inst/shiny/modules/summary_study.R
@@ -1,53 +1,61 @@
 summary_study_module_ui <- function(id) {
   ns <- shiny::NS(id)
   tagList(
-    numericInput(ns("ForestContent"), label="Download text size:", value=12),
-    numericInput(ns("ForestTitle"), label = "Title text size:", value = 1, step = 0.1),
-    numericInput(ns("ForestHeader"), label = "Group headers text size:", value = 1, step = 0.1),
-    radioButtons(ns("format_freq0"), label = 'Document format', choices = c("PDF" = "pdf", "SVG" = "svg"), inline = TRUE),
-    downloadButton(ns("downloadStudy")),
-    actionButton(ns("run"), "Run module summary_study")
+    numericInput(ns("content"), label="Download text size:", value=12),
+    numericInput(ns("title"), label = "Title text size:", value = 1, step = 0.1),
+    numericInput(ns("header"), label = "Group headers text size:", value = 1, step = 0.1),
+    radioButtons(ns("format"), label = "Document format", choices = c("PDF" = "pdf", "SVG" = "svg"), inline = TRUE),
+    downloadButton(ns("download"))
   )
 }
 
 summary_study_module_server <- function(id, common, parent_session) {
   moduleServer(id, function(input, output, session) {
 
-  observeEvent(list(input$run, watch("summary_exclude")), {
-    # WARNING ####
-
-    # FUNCTION CALL ####
-
-    # LOAD INTO COMMON ####
+  observeEvent(list(watch("summary_exclude"), input$content, input$title, input$header), {
 
     # METADATA ####
       common$meta$summary_study$used <- TRUE
-      common$meta$summary_study$ForestContent <- as.numeric(input$ForestContent)
-      common$meta$summary_study$ForestTitle <- as.numeric(input$ForestTitle)
-      common$meta$summary_study$ForestHeader <- as.numeric(input$ForestHeader)
-      common$meta$summary_study$format_freq0 <- input$format_freq0
+      common$meta$summary_study$content <- as.numeric(input$content)
+      common$meta$summary_study$title <- as.numeric(input$title)
+      common$meta$summary_study$header <- as.numeric(input$header)
+      common$meta$summary_study$format <- input$format
 
     # TRIGGER
     trigger("summary_study")
   })
 
-  output$forestPlot <- renderPlot({
+  # Determine the plot height in pixels
+  plot_height <- reactive({
     watch("summary_study")
     req(common$freq_sub)
-    summary_study(common$freq_sub, common$outcome_measure, input$ForestHeader, input$ForestTitle)
+    #The rows that don't correspond to NA treatment effects
+    proper_comparison_rows <- !is.na(common$freq_sub$d0$TE)
+    #The number of comparisons, with NA rows dropped
+    n_proper_comparisons <- length(common$freq_sub$d0$TE[proper_comparison_rows])
+    #The number of unique treatments, with NA rows dropped
+    n_proper_treatments <- length(unique(c(common$freq_sub$d0$treat1[proper_comparison_rows],
+                                           common$freq_sub$d0$treat2[proper_comparison_rows])))
+    return(n_proper_comparisons * 25 + n_proper_treatments * 35)
   })
 
-  output$downloadStudy <- downloadHandler(
+  output$plot <- renderPlot({
+    watch("summary_study")
+    req(common$freq_sub)
+    summary_study(common$freq_sub, common$outcome_measure, input$header, input$title)
+  }, height = function(){plot_height()})
+
+  output$download <- downloadHandler(
     filename = function() {
-      paste0('StudyResults.', input$format_freq0)
+      paste0('MetaInsight_study_results.', input$format)
     },
     content = function(file) {
-      if (input$format_freq0 == "pdf") {
-        pdf(file = file, pointsize = input$ForestContent, width = 8, height = summary_study(common$freq_sub, common$outcome_measure, input$ForestHeader, input$ForestTitle)$size)
+      if (input$format == "pdf") {
+        pdf(file = file, pointsize = input$content, width = 8, height = summary_study(common$freq_sub, common$outcome_measure, input$header, input$title)$size)
       } else {
-        svg(file = file, pointsize = input$ForestContent, width = 8, height = summary_study(common$freq_sub, common$outcome_measure, input$ForestHeader, input$ForestTitle)$size)
+        svg(file = file, pointsize = input$content, width = 8, height = summary_study(common$freq_sub, common$outcome_measure, input$header, input$title)$size)
       }
-      summary_study(common$freq_sub, common$outcome_measure, input$ForestHeader, input$ForestTitle)
+      summary_study(common$freq_sub, common$outcome_measure, input$header, input$title)
       dev.off()
       }
   )
@@ -56,17 +64,18 @@ summary_study_module_server <- function(id, common, parent_session) {
     save = function() {list(
       ### Manual save start
       ### Manual save end
-      ForestContent = input$ForestContent,
-      ForestTitle = input$ForestTitle,
-      ForestHeader = input$ForestHeader)
+      content = input$content,
+      title = input$title,
+      header = input$header,
+      format = input$format)
     },
     load = function(state) {
       ### Manual load start
       ### Manual load end
-      updateNumericInput(session, "ForestContent", value = state$ForestContent)
-      updateNumericInput(session, "ForestTitle", value = state$ForestTitle)
-      updateNumericInput(session, "ForestHeader", value = state$ForestHeader)
-      updateRadioButtons(session, "format_freq0", selected = state$format_freq0)
+      updateNumericInput(session, "content", value = state$content)
+      updateNumericInput(session, "title", value = state$title)
+      updateNumericInput(session, "header", value = state$header)
+      updateRadioButtons(session, "format", selected = state$format)
     }
   ))
 })
@@ -74,16 +83,13 @@ summary_study_module_server <- function(id, common, parent_session) {
 
 summary_study_module_result <- function(id) {
   ns <- NS(id)
-
-  # Result UI
-  # the size of this should adjust depending on the number of studies
-  plotOutput(ns("forestPlot"), height = "1000px", width = "800px")
+  plotOutput(ns("plot"))
 }
 
 summary_study_module_rmd <- function(common){ list(
   summary_study_knit = !is.null(common$meta$summary_study$used),
-  summary_study_ForestContent = common$meta$summary_study$ForestContent,
-  summary_study_ForestTitle = common$meta$summary_study$ForestTitle,
-  summary_study_ForestHeader = common$meta$summary_study$ForestHeader)
+  summary_study_content = common$meta$summary_study$content,
+  summary_study_title = common$meta$summary_study$title,
+  summary_study_header = common$meta$summary_study$header)
 }
 
