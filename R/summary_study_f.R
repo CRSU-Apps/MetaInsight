9c9ebbd23c7defb2646b74b7afb410a75f9521d7
simon-smart88 <simon.smart@cantab.net>
2025-02-26 19:32:19 +0000

    merge groupforest.df

--- R/summary_study_f.R
+++ R/summary_study_f.R
@@ -20,6 +20,60 @@ summary_study <- function(freq, outcome_measure, header, title, logger = NULL) {
     return()
   }
 
-  return(groupforest.df(d1 = freq$d0, ntx = freq$ntx, lstx = freq$lstx, outcome = outcome_measure,
-                        HeaderSize = header, TitleSize = title))
+  d1 = freq$d0
+  ntx = freq$ntx
+  lstx = freq$lstx
+  outcome = outcome_measure
+  HeaderSize = header
+  TitleSize = title
+
+  text_label <- character()
+  n_stud <- integer()
+  for (i in 1:ntx) {
+    for (j in 1:ntx) {
+      if (nrow(d1[(d1$treat1 == i & d1$treat2 == j),]) > 0) {
+        text_label <- c(paste(lstx[i], "vs", lstx[j]), text_label)
+        n_stud <- c(n_stud, nrow(d1[(d1$treat1 == i & d1$treat2 == j),]))
+      }
+    }
+  }
+  gaps <- integer(length(n_stud))
+  n_stud <- rev(n_stud)
+  for (k in 1:length(n_stud)) {
+    if (k == 1) {
+      gaps[k] <- n_stud[k] + 1
+    }
+    else {
+      gaps[k] <- gaps[k-1] + n_stud[k] + 2
+    }
+  }
+  lines <- rev(c(1:(nrow(d1) + 2 * length(text_label) - 1)))
+  lines <- lines[!lines %in% gaps]
+  lines <- lines[!lines %in% (gaps + 1)]
+
+  if (max(lines) < 28) {size = 7
+  } else if (max(lines) >= 28 & max(lines) <= 40) {size = max(lines) / 4
+  } else if (max(lines) > 40 & max(lines) <= 70) {size = max(lines) / 5
+  } else if (max(lines) > 70 & max(lines) <= 100) {size = max(lines) / 6
+  } else if (max(lines) > 100 & max(lines) <= 130) {size = max(lines) / 7
+  } else {size = max(lines) / 8
+  } # sizing for output
+
+  d1 <- d1[order(d1$treat1, d1$treat2, d1$StudyID), ] #ensuring the ordering is correct
+
+  if (outcome == "OR" | outcome =="RR" ){
+    fplot <- metafor::forest(d1$TE, sei = d1$seTE, slab = paste(d1$Study), subset = order(d1$treat1, d1$treat2),
+                             ylim = c(1, nrow(d1) + 2 * length(text_label) + 2), rows = lines, atransf = exp,
+                             at = log(c(0.01, 1, 10, 100)), xlab = paste("Observed ", outcome), efac = 0.5
+    )
+  } else {
+    fplot <- metafor::forest(d1$TE, sei = d1$seTE, slab = paste(d1$Study), subset = order(d1$treat1, d1$treat2),
+                             ylim = c(1, nrow(d1) + 2 * length(text_label) + 2), rows = lines,
+                             xlab = paste("Observed ",outcome), efac = 0.5)
+  }
+  text(fplot$xlim[1], gaps, pos = 4, font = 4, text_label, cex = HeaderSize)
+  title("Individual study results (with selected studies excluded) grouped by treatment comparison", cex.main = TitleSize)
+
+  list(fplot = fplot, size = size)
+
 }
