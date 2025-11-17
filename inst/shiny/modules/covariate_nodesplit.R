d1bbd78d58260f77bbea14b1b9b23c20b064b0c5
simon-smart88 <simon.smart@cantab.net>
2025-11-17 09:17:11 +0000

    add regression nodesplit modules

---
+++ inst/shiny/modules/covariate_nodesplit.R
@@ -0,0 +1,13 @@
+covariate_nodesplit_module_ui <- function(id) {
+  p(
+    "Due to limitations with the underlying R package {gemtc} it is not currently possible to run a regression nodesplit model within MetaInsight.
+   When this functionality becomes available, it will be added here."
+  )
+}
+
+covariate_nodesplit_module_server <- function(id, common, parent_session) {
+  moduleServer(id, function(input, output, session) {
+})
+}
+
+
