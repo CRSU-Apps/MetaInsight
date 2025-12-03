1f24347434fd236e6249a4a26cfe9b070e58d8f9
simon-smart88 <simon.smart@cantab.net>
2025-12-03 17:41:27 +0000

    move / remove old code

--- R/setup_configure_f.R
+++ R/setup_configure_f.R
@@ -228,54 +228,40 @@ InferCovariateType <- function(covariate_data) {
   }
 }
 
+#' Converts wide data to long, adds the variable 'se' for a continuous outcomes, and does some formatting.
+#'
+#' @param newData1 Input dataset.
+#' @param treat_list Data frame containing the treatment ID ('Number') and the treatment name ('Label').
+#' @param CONBI "Continuous" or "Binary".
+#' @return Input data set in long format with the variable 'se' for a continuous outcome.
+dataform.df <- function(newData1, treat_list, CONBI) {
+  if (FindDataShape(newData1) == "long") {
+    long <- newData1
+  } else {
+    long <- WideToLong(wide_data = newData1, outcome = CONBI)
+  }
 
+  long_sort <- long[order(long$StudyID, -long$T), ]
+  if (CONBI == 'Continuous') {
+    long_sort$se <- long_sort$SD / sqrt(long_sort$N)
+  }
 
-#' #' Validate the covariate and infer the type of the covariate from the data in the column.
-#' #'  In error cases, this function will throw exceptions:
-#' #' - If the data has any NAs
-#' #' - If the data has any non-numeric values
-#' #' - If every study has the same covariate value
-#' #' - If any study contains multiple different covariate values
-#' #'
-#' #' @param data Data frame containing all study data.
-#' #' @param covariate_title Name of the covariate column.
-#' #'
-#' #' @return "binary" if covariate has only 0 & 1 as numeric values,
-#' #' "continuous" if covariate has more than 2 numeric values
-#' ValidateAndInferCovariateType <- function(data, covariate_title) {
-#'   covariate_data <- data[[covariate_title]]
+  long_sort <- ReinstateTreatmentIds(data = long_sort, treatment_ids = treat_list)
+
+  return(long_sort)
+}
+
+#' Reformats the treatment labels then calls dataform.df().
 #'
-#'   if (!is.numeric(covariate_data)) {
-#'     stop("One or more covariate values are non-numerical.")
-#'   }
-#'
-#'   covariate_values <- list()
-#'   for (study in unique(data$Study)) {
-#'     covariate_values[[study]] <- unique(covariate_data[data$Study == study])
-#'   }
-#'
-#'   .ThrowErrorForMatchingStudies(
-#'     values = covariate_values,
-#'     condition = function(study_values) {
-#'       any(is.na(study_values))
-#'     },
-#'     message = "Some studies do not define covariate values for all arms:"
-#'   )
-#'
-#'   .ThrowErrorForMatchingStudies(
-#'     values = covariate_values,
-#'     condition = function(study_values) {
-#'       length(study_values) > 1
-#'     },
-#'     message = "Some studies contain inconsistent covariate values between arms:"
-#'   )
-#'
-#'   unique_items <- unique(covariate_data)
-#'   if (length(unique_items) == 1) {
-#'     stop("Cannot analyse covariate with no variation.")
-#'   } else if (length(unique_items) == 2 && all(sort(unique_items) == c(0, 1))) {
-#'     return("Binary")
-#'   }
-#'
-#'   return("Continuous")
-#' }
+#' @param data Input dataset.
+#' @param metaoutcome "Continuous" or "Binary".
+#' @param treatment_list Data frame containing the treatment ID ('Number') and the treatment name ('Label').
+#' @return Output from dataform.df().
+bugsnetdata <- function(data, metaoutcome, treatment_list){
+  newData1 <- as.data.frame(data)
+  treatment_list$Label <- stringr::str_wrap(gsub("_", " ", treatment_list$Label), width = 10)  # better formatting (although does assume underscores have only been added due to the treatment label entry limitations) CRN
+  longsort2 <- dataform.df(newData1 = newData1,
+                           treat_list = treatment_list,
+                           CONBI = metaoutcome)
+  return(longsort2)
+}
