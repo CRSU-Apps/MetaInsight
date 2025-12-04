a4425caa8b4508bb31571e9e950a8f6ff14d39c0
simon-smart88 <simon.smart@cantab.net>
2025-12-04 12:13:48 +0000

    refactor network.structure to native pipes, document and move to ranking function

--- R/bayes_ranking_f.R
+++ R/bayes_ranking_f.R
@@ -425,3 +425,97 @@ ranking_table = function(ranking_data) {
   df <- df[order(-df$SUCRA),]
   return(df)
 }
+
+#' Function taken and adapted from BUGSnet GitHub
+#' @param data.nma BUGSnetData item, created using `data.prep(arm.data=longdata, varname.t = "T", varname.s="Study")`
+#' @param my_order character. Vector of treatments names in rank order.
+#' @return data.frame containing the number of studies that compare each treatment against the reference treatment.
+network.structure <- function(data.nma, my_order = NA) {
+
+  # Bind Variables to function
+  from <- NULL
+  to <- NULL
+  trt <- NULL
+  flag <- NULL
+  mtchvar <- NULL
+  trial <- rlang::quo(!!as.name(data.nma$varname.s))
+  varname.t.quo <- rlang::quo(!!as.name(data.nma$varname.t))
+
+  # Change underscores if present (as when my_order is based on rank results, it'll already have underscores removed)
+  data.nma$arm.data$T <- data.nma$arm.data$T
+  data.nma$treatments$T <- data.nma$treatments$T
+
+  studytrt <- data.nma$arm.data |>
+    dplyr::select(data.nma$varname.s, data.nma$varname.t) |>
+    tidyr::nest(data = c(data.nma$varname.t)) # nest treatments within each study
+
+  cnt <- data.nma$arm.data |>
+    dplyr::select(data.nma$varname.s, data.nma$varname.t) |>
+    dplyr::count(data.nma$varname.s) # number of treatments within each study
+
+  tmp1 <- dplyr::bind_cols(studytrt, cnt) |>
+    dplyr::filter(n > 1) # removes single arm studies
+
+  if (rlang::is_empty(my_order)) {
+    pairs <- tmp1[1, "data"] |>
+      unlist() |>
+      sort() |>
+      combn(2) # each set of treatment pairs is put in alphabetical order
+  } else {
+    pairs <- tmp1[1, "data"] |>
+      unlist() |>
+      (\(x) x[order(match(x, my_order))])() |>
+      combn(2) # orders according to 'my_order'
+  }
+
+  for(i in 2:nrow(tmp1)){
+    if (rlang::is_empty(my_order)) {
+      pairs <- tmp1[i, "data"] |>
+        unlist() |>
+        sort() |>
+        combn(2) |>
+        cbind(pairs)
+    } else {
+      pairs <- tmp1[i, "data"] |>
+        unlist() |>
+        (\(x) x[order(match(x, my_order))])() |>
+        combn(2) |>
+        cbind(pairs)
+    }
+  }
+
+  # data of each pairwise comparison and number of trials
+  pairs2 <- data.frame(
+    from = pairs[1, ],
+    to = pairs[2, ]) |>
+    dplyr::group_by(from, to) |>
+    dplyr::mutate(edge.weight = max(1:dplyr::n())) |>
+    dplyr::arrange(from, to) |>
+    dplyr::distinct() |>
+    dplyr::mutate(mtchvar = 1)
+
+  studylabs <- studytrt |>
+    dplyr::group_by(!! trial) |>
+    dplyr::mutate(trt = paste(unlist(data), collapse = ';')) |> # counts number of pairwise comparisons
+    dplyr::select(!!trial, trt) |>
+    dplyr::mutate(mtchvar = 1)
+
+  edges <- dplyr::left_join(pairs2, studylabs, by = "mtchvar", relationship = "many-to-many") |>
+    dplyr::ungroup() |>
+    dplyr::mutate(
+      flag = ifelse(
+        stringr::str_detect(trt, stringr::coll(from)) & stringr::str_detect(trt, stringr::coll(to)),
+        1,
+        0
+      )
+    ) |>
+    dplyr::filter(flag == 1) |>
+    dplyr::select(-c(mtchvar, flag, trt)) |>
+    tidyr::nest(data = c(!!trial)) |> # nests the studies for which belonged to each treatment comparison
+    dplyr::group_by(from, to) |>
+    dplyr::mutate(study = paste(unlist(data), collapse = ', \n')) |>
+    dplyr::select(-data)
+
+  return(edges)
+}
+
