4474fc16da0b980405fc673ff1b61a1bcee5f822
simon-smart88 <simon.smart@cantab.net>
2026-01-19 18:53:37 +0000

    remove duplicate functions

--- R/baseline_regression_f.R
+++ R/baseline_regression_f.R
@@ -149,43 +149,5 @@ CalculateCredibleRegionsBnma <- function(model_output) {
   )
 }
 
-#' Puts a relative effects table from bnma into gemtc format
-#'
-#' @param median_ci_table Output from bnma::relative.effects.table(, summary_stat = "ci")
-#' @return A relative effects table in the same format as from gemtc.
-BaselineRiskRelativeEffectsTable <- function(median_ci_table) {
-  #Entries in the input table are in the form "[lower_ci,median,upper_ci]" (no spaces)
 
-  #The dimensions of the (square) table
-  dim_median <- nrow(median_ci_table)
-  #Create matrices to store the lower_ci, median and upper_ci separately
-  lower_ci <- matrix(nrow = dim_median, ncol = dim_median)
-  median_br <- matrix(nrow = dim_median, ncol = dim_median)
-  upper_ci <- matrix(nrow = dim_median, ncol = dim_median)
-
-  for (row in 1:dim_median) {
-    for (col in 1:dim_median) {
-      #Extract lower_ci, median and upper_ci
-      interval <- round(
-        as.numeric(
-          stringr::str_extract_all(
-            string = median_ci_table[row, col],
-            pattern = "[-0-9\\.]+")[[1]]
-        ),
-        digits = 2
-      )
-      lower_ci[row, col] <- interval[1]
-      median_br[row, col] <- interval[2]
-      upper_ci[row, col] <- interval[3]
-    }
-  }
-
-  #Paste into the format "median (lower_ci, upper_ci)"
-  median_ci_table_new <- matrix(paste0(median_br, " (", lower_ci, ", ", upper_ci, ")"), nrow = dim_median)
-  diag(median_ci_table_new) <- rownames(median_ci_table)
-  rownames(median_ci_table_new) <- rownames(median_ci_table)
-  colnames(median_ci_table_new) <- colnames(median_ci_table)
-
-  return(median_ci_table_new)
-}
 
