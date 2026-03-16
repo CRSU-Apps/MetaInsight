b3e38a3f09d0972414852c75dff18159270f03c8
Tom Morris <tommorris168@gmail.com>
2026-03-16 09:54:19 +0000

    Better reversing

--- R/freq_analysis.R
+++ R/freq_analysis.R
@@ -18,9 +18,10 @@ frequentist <- function(non_covariate_data, outcome, treatments, outcome_measure
 
   # transform data to contrast form
   pairwise <- contrastform.df(wide_data, outcome_measure, outcome)
+  reverse_pairwise <- contrastform.df(wide_data, outcome_measure, outcome, reverse = TRUE)
   # matching treatment labels to treatment code
   labelled_pairwise <- labelmatching.df(pairwise, treatments)
-  reverse_pairwise <- reverse_pairwise(labelled_pairwise)
+  labelled_reverse_pairwise <- labelmatching.df(reverse_pairwise, treatments)
 
   netmeta <- netmeta::netmeta(TE = labelled_pairwise$TE,
                           seTE = labelled_pairwise$seTE,
@@ -45,18 +46,26 @@ frequentist <- function(non_covariate_data, outcome, treatments, outcome_measure
   return(list(
     netmeta = netmeta,
     pairwise = labelled_pairwise,
-    reverse_pairwise = reverse_pairwise))
+    reverse_pairwise = labelled_reverse_pairwise))
 
 }
 
 #' Puts data in contrast form using netmeta::pairwise().
 #'
 #' @param wide_data non-covariate data in a wide format
+#' @param reverse Reverses the order of the treatments if TRUE. Defaults to FALSE.
 #' @inheritParams common_params
 #' @return Input data in contrast form.
 #' @noRd
-contrastform.df <- function(wide_data, outcome_measure, outcome) {
+contrastform.df <- function(wide_data, outcome_measure, outcome, reverse = FALSE) {
 
+  #Selects the reverse function 'rev' or the identity function 'c'.
+  ReverseFunction <- switch(
+    as.character(reverse),
+    "TRUE" = rev,
+    "FALSE" = c
+  )
+  
   #Create a list of columns of the variables to be passed to meta::pairwise()
   treat_list <- CreateListOfWideColumns(wide_data = wide_data, column_prefix = "T")
   n_list <-  CreateListOfWideColumns(wide_data = wide_data, column_prefix = "N")
@@ -66,23 +75,28 @@ contrastform.df <- function(wide_data, outcome_measure, outcome) {
     mean_list <-  CreateListOfWideColumns(wide_data = wide_data, column_prefix = "Mean")
     sd_list <-  CreateListOfWideColumns(wide_data = wide_data, column_prefix = "SD")
 
-    d1 <- meta::pairwise(treat = treat_list,
-                         n = n_list,
-                         mean = mean_list,
-                         sd = sd_list,
-                         data = wide_data,
-                         sm = outcome_measure,
-                         studlab = wide_data$Study)
+    d1 <- meta::pairwise(
+      treat = ReverseFunction(treat_list),
+      n = ReverseFunction(n_list),
+      mean = ReverseFunction(mean_list),
+      sd = ReverseFunction(sd_list),
+      data = wide_data,
+      sm = outcome_measure,
+      studlab = wide_data$Study
+    )
+
   } else if (outcome == 'binary') {
 
     event_list <-  CreateListOfWideColumns(wide_data = wide_data, column_prefix = "R")
 
-    d1 <- meta::pairwise(treat = treat_list,
-                         event = event_list,
-                         n = n_list,
-                         data = wide_data,
-                         sm = outcome_measure,
-                         studlab = wide_data$Study)
+    d1 <- meta::pairwise(
+      treat = ReverseFunction(treat_list),
+      event = ReverseFunction(event_list),
+      n = ReverseFunction(n_list),
+      data = wide_data,
+      sm = outcome_measure,
+      studlab = wide_data$Study
+    )
   } else {
     stop("outcome must be 'continuous' or 'binary'")
   }
@@ -105,41 +119,5 @@ labelmatching.df <- function(pairwise, treatments) {
   pairwise$treat2 <- factor(pairwise$treat2,
                       levels = 1:ntx,
                       labels = as.character(treatments$Label))
-  return(pairwise)
-}
-
-#' Reverses the pairwise data frame created by meta::pairwise, as if the treatments had been the other way round.
-#' @param pairwise The results of meta::pairwise with labelled treatments.
-#' @inheritParams common_params
-#' @return Data frame
-#' @noRd
-reverse_pairwise <- function(pairwise) {
-  pairwise$TE <- -pairwise$TE
-
-  treat1_original <- pairwise$treat1
-  pairwise$treat1 <- pairwise$treat2
-  pairwise$treat2 <- treat1_original
-
-  T1_original <- pairwise$T.1
-  pairwise$T.1 <- pairwise$T.2
-  pairwise$T.2 <- T1_original
-
-  N1_original <- pairwise$N.1
-  pairwise$N.1 <- pairwise$N.2
-  pairwise$N.2 <- N1_original
-
-  if (is.element("R.1", names(pairwise))) {
-    R1_original <- pairwise$R.1
-    pairwise$R.1 <- pairwise$R.2
-    pairwise$R.2 <- R1_original
-  } else {
-    Mean1_original <- pairwise$Mean.1
-    pairwise$Mean.1 <- pairwise$Mean.2
-    pairwise$Mean.2 <- Mean1_original
-
-    SD1_original <- pairwise$SD.1
-    pairwise$SD.1 <- pairwise$SD.2
-    pairwise$SD.2 <- SD1_original
-  }
   return(pairwise)
 }
