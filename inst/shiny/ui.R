c83122ed728ed4e8d7ee136c1fe3c49f10e6614c
simon-smart88 <simon.smart@cantab.net>
2026-04-29 15:59:17 +0100

    fix typo

--- inst/shiny/ui.R
+++ inst/shiny/ui.R
@@ -25,7 +25,7 @@ tagList(
       includeHTML(file.path(resourcePath, "favicon", "favicon.html")),
       tags$meta(name="description", content="A interactive web tool for network meta-analysis (NMA) that leverages established analysis routines"),
       tags$meta(name="keywords", content="MetaInsight, NMA, Network, Meta, Analysis, App"),
-      tags$meta(property="og:title", content="Meta Insight: v7.0.0"),
+      tags$meta(property="og:title", content="MetaInsight: v7.0.0"),
       tags$meta(property="og:description", content="An interactive web tool for network meta-analysis (NMA) that leverages established analysis routines"),
       tags$meta(property="og:image", content="https://raw.githubusercontent.com/CRSU-Apps/MetaInsight/main/www/images/MetaInsightLogo.png"),
       core_analytics_module_ui("core_analytics")
