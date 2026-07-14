f740a4297785837994b72edb9dde4e3494e3da8f
Ryan Field <RyanJField@users.noreply.github.com>
2026-07-14 21:53:31 +0100

    Override SVG code

--- R/helper_functions.R
+++ R/helper_functions.R
@@ -333,10 +333,10 @@ format_step <- function(x){
 #' @noRd
 crop_svg <- function(svg, margin = 10){
 
-  svg_node <- shiny::HTML(paste(svg, collapse = "\n")) |>
+  svg <- shiny::HTML(paste(svg, collapse = "\n")) |>
     xml2::read_html() |>
     xml2::xml_find_first("//svg")
-  pixel_data <- paste(svg_node, collapse = "\n") |>
+  pixel_data <- paste(svg, collapse = "\n") |>
     charToRaw() |>
     rsvg::rsvg_raw()
 
